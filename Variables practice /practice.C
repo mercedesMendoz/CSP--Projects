06ada50afee4b230d49ede5935eb23717ee55973
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-09-11 03:55:32 +0000

    made changes to variables

--- Variables practice /practice.C
+++ Variables practice /practice.C
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Hello World");
+int main(void) {
+    char name[ ] = "Mercedes";
+    printf("%s",name);
+
+char number[ ] = "7";
+    printf("%s",number);
+
+    char number[ ] = "300";
+    printf("%s",number);
+
+     char breakfast[ ] = "eggs and bacon";
+    printf("%s",breakfast);
+
+char color[ ] = "pink";
+    printf("%s",color);
+
+    char name [ ] = "UCAS";
+    printf("%s",name);
+
+     char year[ ] = "2024";
+    printf("%s",year);
+
+     char color[ ] = "brown";
+    printf("%s",color);
+
+     char age[ ] = "14";
+    printf("%s",age);
+
+     char subject[ ] = "english";
+    printf("%s",subject);
+
+
+
+
+
+
+
+
+
     return 0;
 }
