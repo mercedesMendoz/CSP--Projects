65df9dd7433d1f3ed5353b426a28cb9088c81afa
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-09-13 03:38:57 +0000

    did the assingment first program.py!!

---
+++ Variables practice /hello world.py
@@ -0,0 +1,5 @@
+# Ask the user to input their name
+user_name = input("Please enter your name: ")
+
+# Print a personalized greeting
+print(f"Hello, {user_name}! Welcome to my program.")
---
+++ expressions/buget.c
@@ -0,0 +1,11 @@
+#include <stdio.h>>
+
+int main(void){
+    float income,rent,utilities,groceries,transportation,expenses,
+    float ,Pincome,prent,putilities,pgroceries,ptranspertation,pexpenses
+    printf("This is going to calculate your buget for one month");
+    printf("how much you make a month\n");
+    scanf("%f",)
+    printf("Your income is : $%.2f\n", income );
+    return 0;
+}
