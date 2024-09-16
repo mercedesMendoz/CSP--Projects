5f22429a3d9c9b95e823135bd16bb2b177770033
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-09-16 20:42:14 +0000

    made more additions to the code

--- expressions/buget.c
+++ expressions/buget.c
@@ -5,7 +5,31 @@ int main(void){
     float ,Pincome,prent,putilities,pgroceries,ptranspertation,pexpenses
     printf("This is going to calculate your buget for one month");
     printf("how much you make a month\n");
-    scanf("%f",)
-    printf("Your income is : $%.2f\n", income );
+    scanf("%f" ,&income)
+    printf("Your income is : $%.2f\n", income);
+    printf("How much do you pay for rent");
+    scanf("%f",&rent);
+    printf("How much do you pay for utilites");
+    scanf("%f",&utilities);
+    printf("How much do you pay for groceries");
+    scanf("%f",&groceries);
+    printf("How much do you pay for transportation");
+    scanf("%f" ,&transportation)
+    printf("How much do you pay for expenses");
+    scanf("%f" ,&expenses);
+     expenses = rent, utilities, groceries, transportation,expenses;
+
+       
+       
+
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
+
 }
