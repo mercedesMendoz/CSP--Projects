e1904bed276c75db8bc598567e808ca6c9f5b08c
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-09-17 04:09:01 +0000

    finished buget c coding assingment

--- expressions/buget.c
+++ expressions/buget.c
@@ -1,8 +1,8 @@
 #include <stdio.h>>
 
 int main(void){
-    float income,rent,utilities,groceries,transportation,expenses,
-    float ,Pincome,prent,putilities,pgroceries,ptranspertation,pexpenses
+    float income,rent,utilities,groceries,transportation,expenses,savings
+    float ,Pincome,prent,putilities,pgroceries,ptranspertation,pexpenses,psavings
     printf("This is going to calculate your buget for one month");
     printf("how much you make a month\n");
     scanf("%f" ,&income)
@@ -15,21 +15,24 @@ int main(void){
     scanf("%f",&groceries);
     printf("How much do you pay for transportation");
     scanf("%f" ,&transportation)
-    printf("How much do you pay for expenses");
-    scanf("%f" ,&expenses);
-     expenses = rent, utilities, groceries, transportation,expenses;
-
-       
-       
-
-    
-    
-    
-    
-    
-    
-    
-    
+    savings = income * .2;
+    expenses = rent + utilities + groceries + transportation;
+    spend = income - expenses - savings;
+    printf ("You make $%.2f\n", expenses);
+    printf ("Your savings are $%.2f\n," savings);
+    printf ("The amount of money you have left is $%.2f\n", spend);
+    prent = rent/income *100;
+    putilities = utilities/income *100;
+    pgroceries = groceries/income *100;
+    ptransportation = transportation/income *100;
+    psavings = savings/income *100;
+    pexpenses = expenses/income *100;
+    printf("Your rent is %d%% of your income \n", (int) prent);
+    printf (" Your utilities is %d%% of your income \n", (int) putilities);
+    printf ("Your groceries is %d%% of your income \n", (int) pgroceries);
+    printf ("Your transportation is %d%% of your income \n", (int) ptransportation);
+    printf("Your savings is %d%% of your income \n", (int) psavings);
+    printf("Your expenses is %d%% of your income \n", (int) pexpenses);  
     return 0;
 
 }
