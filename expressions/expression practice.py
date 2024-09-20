ba2f818182e44c5eb2edd65188243c528f3fa357
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-09-20 20:42:13 +0000

    starting assingment exponents practice

---
+++ expressions/expression practice.py
@@ -0,0 +1,12 @@
+exponent = 7-24/8*4+6
+print(exponent)
+exponent = 18/3-7+2*5
+print(exponent)
+exponent = 6*4/12+72/8-9
+print(exponent)
+exponent = (17-6/2)+4*3
+print(exponent)
+exponent = -2*(1*4-2/2)+(6+2-3)
+print(exponent)
+exponent = -1*((3-4*7))/5-2*24/6
+print(exponent)
