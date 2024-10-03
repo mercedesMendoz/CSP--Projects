e25af5316529005840469351b7221858040fd19f
mercedesMendoz <mercedes.mendoza@ucas-edu.net>
2024-10-03 04:31:54 +0000

    finished silly scentences PYTHON!!!!

--- Strings/name decorator.c
+++ Strings/name decorator.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
-
-int main(void){
-    char name[20]
-    printf("type your name");
-    scanf("%s",name);
-    strcat(name, "<<<>>>");
-    printf ("");
-    return 0;
+#include «string.h>
+int main(void char name(30):
+char dec10ne(30) - *ecc N
+char dec1Two[30] - "»o*;
+char dec20ne(30) - *(I*:
+char dec2Two{30] - ^ J/:
+char dec30ne[30] - "...* char dec3Two(30] - *-*
+char dec40ne(30) = *C:C*:
+char dec4Two(30] = "3) :):
+char decOne(30) = ***#*:
+char decSTwo(30) = * anu";
+char dec6One(30) •*+*+*
+char dec6Two[30] - *****
+char dec7One(30] -*-
+char dec7Two[30] - *
+char decOne(30] - *
+char dec8Two[30] -**==*
+print/("What is your name?" (gets(name, sizeof(name), stdin):
+size_t len = strlen(name):
+if (len > 0 8& namellen - 1] == 5n7€
+name(len - 1) = 10';
+strcat(dec10ne,name);
+strcat(dec10ne,dec1Two):
+strcat(dec20ne,name):
+strcat(dec20ne,dec2Two):
+strcat(dec30ne,name):
+strcat(dec30ne,dec3Two)
+strcat(dec40ne,name):
+strcat(dec40ne,decaTwo):
+strat(dec50ne, name);
+strcat(dec50ne,decSTwo):
+strcat(dec6One,name):
+strcat(dec60ne,dec6To):
+stecat(dec70ne name);
+sucat(dec70ne.dec71wo):
+Strcat(dec80ne.dec8Two):
+printf(*%sin" dect Onel:
+printf( "gesis" decOne):
+printi("%sin* deciOnel:
+print (asint decone)
+printff("gisin* dec6One);
+printi*osin* dec/Onel;
+printi("9sin* decBOne):
 }
